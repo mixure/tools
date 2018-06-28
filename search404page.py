bcccd21880b2831a8c33636a3653bd7de33822df
zhoucheng <yeap@yeap-MacBook-Pro.local>
2018-06-29 07:10:41 +0800

    添加非<a href=xxx>时获取超链接

--- search404page.py
+++ search404page.py
@@ -7,10 +7,15 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from logzero import logger
 
-domain="https://www.yirendai.com/abc"
-title404="您访问的页面不存在"
+domain="https://www.9douyu.com"
+# domain='http://app.qyer.com/guide/'
+title404="404错误页面"
 page_size404=1308
-dr=webdriver.Firefox()
+dr=webdriver.Safari()
+dr.get('https://www.9douyu.com/login')
+dr.find_element('id','username').send_keys('')
+dr.find_element('id','password').send_keys('')
+dr.find_element('id','v4-input-btn').click()
 
 def scrap_url(url,orginal_url):
     status_code=200
@@ -37,10 +42,16 @@ def scrap_url(url,orginal_url):
     more_seeds=[]
     #有些网站的站内的超链接不是href参数提供的,需要继续在if修改
     # 有些还有外链，都是在这处理....
-    if True or status_code==200:  # 有些网站404页面有本站链接,比如宜信
+    if status_code==200:  # 有些网站404页面有本站链接,比如宜信
         for a in soup.find_all(href=True):
             if a['href'].startswith('/'):
                 more_seeds.append((urljoin(domain,a['href']),url))
+        """
+        for a in soup.find_all('a'): #非<a href=xxx>处理
+            for k,v in a.attrs.items():
+                if v[0]=='/':
+                    logzero(v)
+            """
     return more_seeds
 
 def main(domain):
@@ -49,7 +60,9 @@ def main(domain):
     while seeds:
         seed,*seeds=seeds
         url,orginal_url=seed
-
+        if 'tmail' in url :continue
+        if 'qyer-release-android' in url:continue
+        if 'apple-store' in url:continue
         if url in old_urls:
             logger.debug(f'{url}, 从{orginal_url}获取该链接,不进行重复访问 ')
         else:
