87776a0d9a5a5754f656a44b9f4e8544111007c3
mixure <easy_install@163.com>
2018-11-19 11:07:44 +0800

    Update ali_centos7.4_env_set.sh

--- ali_centos7.4_env_set.sh
+++ ali_centos7.4_env_set.sh
@@ -1,70 +1,70 @@
 #! /bin/bash
+#
 
-# 'source xxx.sh' run this script 
+#
+echo "LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8">>/etc/environment
 
-profile=/etc/profile
+#
+yum -y install git
+sed -i "\$a vi=vim" $HOME/.bashrc
+sed -i "\$a h=history" $HOME/.bashrc
+echo 'alias vi=vim'>>~/.bashrc
 
-# solving "-bash: warning: setlocale: LC_CTYPE: cannot change locale (UTF-8): No such file or directory" alert
-echo 'LC_ALL=en_US.UTF-8
-LANG=en_US.UTF-8'>>/etc/environment
-
-# set Java
-jdk=`ls |grep jdk`
+# jdk
+jdk=`rpm -qa|grep openjdk`
+if test -n "$jdk";then
+        echo -e "Remove openjdk manually (y|Y) ?, other input to ignore it. "
+        read ans
+        [ "$ans" == "y" -o "$ans" == "Y" ] && echo 0 
+fi
+jdk=`ls|grep jdk`
 tar -xvf $jdk
-jdk=`ls |grep jdk[0-9]`
-cp -r $jdk /usr/local/
-echo "export JAVA_HOME=/usr/local/$jdk" >> $profile
-sed -i '$a export PATH=$PATH:${JAVA_HOME}/bin' $profile
-ln -s /usr/local/$jdk/bin/java /usr/bin/java # jenkins use /usr/bin/java
-rm -rf $jdk
-unset jdk
+jdk=`ls|grep jdk.*[0-9]$`
+mv $jdk /usr/local/
+sed -i "\$a export JAVA_HOME=/usr/local/${jdk}" /etc/profile
+sed -i '$a export PATH=$PATH:$JAVA_HOME/bin' /etc/profile
+ln -s /usr/local/$jdk/bin/java /usr/bin/java
 
-# set jenkins
-jenkins=`ls|grep jenk`
-rpm -ivh $jenkins
-/etc/init.d/jenkins start
-unset jenkins
+# jenkins
+jenkins=`ls|grep jenkins`
+rpm -ivh $jenkins --nosignature
+sed -i 's/JENKINS_USER="jenkins"/JENKINS_USER="root"/g' /etc/sysconfig/jenkins
+systemctl start jenkins
+/sbin/chkconfig jenkins on
 
-# set Python
+# python
 yum -y install ncurses ncurses-devel  
-yum install openssl openssl-devel -y
+yum -y install openssl openssl-devel
 python=`ls|grep Python`
 tar -xvf $python
 python=`ls|grep Python.*[0-9]$`
-cd $python&&./configure --prefix=/usr/local/$python
-
-setup=Modules/Setup
-sed -i 's/#_socket socketmodule.c/_socket socketmodule.c/g' $setup
-l=`grep -n '#_ssl _ssl.c' $setup  |cut -d ':' -f 1` # awsome...... god.
-txt=`sed -n "$l,$[$l+2]p" $setup`
-sed -i "$l,$[l+2]d" $setup
-txt=`echo $txt|sed 's/#//g'`
-sed -i "${l}a $txt" $setup
-
+cd $python
+./configure --prefix=/usr/local/$python
+sed -i "s/#_socket socketmodule.c/_socket socketmodule.c/g" Module/Setup
+ssl='_ssl _ssl.c 	-DUSE_SSL -I$(SSL)/include -I$(SSL)/include/openssl -L$(SSL)/lib -lssl -lcrypto'
+sed -i "209a $ssl" Module/Setup 
 make&&make install
-echo 'export PATH=$PATH:'"/usr/local/${python}/bin">> $profile
-cd -
-rm -rf $python
-unset setup
-unset python
+echo 'export PATH=$PATH:'"/usr/local/$python/bin">> /etc/profile
+cd -&& rm -rf $python
 
-# set rvm
+# rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
-echo 'export PATH=$PATH:/usr/local/rvm/bin'>> $profile
+echo 'export PATH=$PATH:/usr/local/rvm/bin'>> /etc/profile
 
-# set vim
-echo 'alias vi=vim'>>~/.bashrc
-yum -y install git
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim
 cp vimrc ~/.vimrc
-echo '------------------------------------------------'
-echo -e "run ':PluginInstall' using vim at the first time"
-echo '------------------------------------------------'
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#
-echo 'alias h=history'>>~/.bashrc
+# mldonkey
+mldonkey=`ls|grep mldonkey`
+cp -r $mldonkey /usr/local
+cd /usr/local/$mldonkey
+echo y|./configure
+gmake
+cp /usr/local/$mldonkey/mlnet /usr/bin/mlnet
 
-#
-. $profile&&. ~/.bashrc
-unset profile
+echo -e "启动mlnet后, 在~/.mldonkey/downloads.ini 修改downloads.ini=["0.0.0.0/0";]\n"
+echo -e "退出后登陆获取环境变量\n"
+echo -e "第一次使用vim需运行 :PlugnInstall安装插件\n" # 只有使用vim命令时，才能识别 :PlugnInstall命令, 所以使用vi时，需要让vi=vim生效
