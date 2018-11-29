cba8d4491a5fa4aa0bb0cae3846c2fcfc34f92c6
mixure <easy_install@163.com>
2018-11-29 13:24:02 +0800

    Update ali_centos7.4_env_set.sh

--- ali_centos7.4_env_set.sh
+++ ali_centos7.4_env_set.sh
@@ -1,24 +1,6 @@
 #! /bin/bash
-#
 
-#
-yum -y install git
-
-#
-echo "alias vi=vim">>$HOME/.bashrc
-echo "alias h=histroy">>$HOME/.bashrc
-
-#
-echo "LC_ALL=en_US.UTF-8
-LANG=en_US.UTF-8">>/etc/environment
-
-# jdk, openjdk的判断没用，压根系统默认不携带
-openjdk=`rpm -qa|grep openjdk`
-if test -n "$openjdk" ;then
-	read -p "1. Remove openjdk manually (y|Y)?
-2. Other key to ignore it. " ans
-	[ "$ans" == "Y" -o "$ans" == "y" ] && exit 0
-fi
+# jdk
 jdk=$(ls|grep jdk)
 tar -xvf $jdk
 jdk=`ls|grep jdk.*[0-9]$`
@@ -42,32 +24,8 @@ tar -xvf $python
 python=`ls|grep Python.*[0-9]$`
 cd $python
 ./configure --prefix=/usr/local/${python}
-sed -i '205s/#//g' Modules/Setup
-sed -i '210,212s/#//g' Modules/Setup
+sed -i '205s/#//g;210,212s/#//g' Modules/Setup
 make && make install
 sed -i '$a export PATH=$PATH:'"/usr/local/$python/bin" /etc/profile
 cd -
 rm -rf $python
-
-# rvm
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
-echo 'export PATH=$PATH:/usr/local/rvm/bin'>>/etc/profile
-
-# vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp vimrc ~/.vimrc  # 第一次执行使用 :PluginInstall 安装插件；:PluginInstall 只在 vim中可使用
-
-# docker
-yum -y install docker
-systemctl  start docker.service
-systemctl  enable docker.service
-
-# mldonkey
-mldonkey=`ls|grep mldonkey`
-mv  $mldonkey /usr/local/
-cd /usr/local/$mldonkey
-./configure  # 编译过程中需要手动输入y
-gmake
-cp /usr/local/$mldonkey/mlnet /usr/bin/mlnet # mlnet启动后, 生成 ~/.mldonkey/download.ini 中修改allow_ip 可外网访问
-
