ab1bd0a135aac0e686076fa8c80b7ce570516c38
mixure <easy_install@163.com>
2019-05-09 14:35:48 +0800

    Create 包装BaiduPCS-Go查看和下载.rb

---
+++ 包装BaiduPCS-Go查看和下载.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/env ruby
+#  coding=utf-8
+
+dir=File.join '.','BaiduPCS-Go'
+
+case ARGV[0]
+when 'ls'
+  `#{dir} config set -appid=266719`
+  `#{dir} #{ARGV.join " "}`.display
+when 'd','download'
+  `#{dir} config set -appid=265486`
+  "运行 #{dir} #{ARGV.join ' '} 进行下载;\n直接调不显示下载进度，忍不了\n".display
+else
+  "不要用我\n".display
+end
