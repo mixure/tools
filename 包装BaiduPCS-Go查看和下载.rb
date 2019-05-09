0903e90fabaeaa36d52d784881c246a5c49d07b6
mixure <easy_install@163.com>
2019-05-09 19:18:02 +0800

    Update 包装BaiduPCS-Go查看和下载.rb

--- 包装BaiduPCS-Go查看和下载.rb
+++ 包装BaiduPCS-Go查看和下载.rb
@@ -4,10 +4,10 @@
 dir=File.join '.','BaiduPCS-Go'
 
 case ARGV[0]
-when 'ls'
+when %{ls}
   `#{dir} config set -appid=266719`
   `#{dir} #{ARGV.join " "}`.display
-when 'd','download'
+when %{d},%{download}
   `#{dir} config set -appid=265486`
   "运行 #{dir} #{ARGV.join ' '} 进行下载;\n直接调不显示下载进度，忍不了\n".display
 else
